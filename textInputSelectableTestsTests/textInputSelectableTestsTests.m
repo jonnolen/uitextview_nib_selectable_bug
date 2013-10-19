674281373286299b59721c48f39fe1f060c6ccfc
Jon Nolen <jon.nolen@gmail.com>
2013-10-19 09:46:35 -0400

    creating tests to demonstrate issue.

--- textInputSelectableTestsTests/textInputSelectableTestsTests.m
+++ textInputSelectableTestsTests/textInputSelectableTestsTests.m
@@ -8,16 +8,18 @@
 
 #import <XCTest/XCTest.h>
 
-@interface textInputSelectableTestsTests : XCTestCase
-
+@interface CodeInitiatedUITextViewTests : XCTestCase{
+    UITextView *textView;
+}
 @end
 
-@implementation textInputSelectableTestsTests
+@implementation CodeInitiatedUITextViewTests
 
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+    textView.attributedText = [[NSAttributedString alloc] initWithString:@"Hello World"];
 }
 
 - (void)tearDown
@@ -26,9 +28,94 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testBeginingOfDocumentPositionShouldNotBeNil
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertNotNil(textView.beginningOfDocument, @"");
+}
+
+- (void)testGetPositionofTextReturnsNotNil{
+    XCTAssertNotNil([textView closestPositionToPoint:CGPointMake(48, 12)], @"");
+}
+
+- (void)testGetOffsetNot0{
+    UITextPosition *pos = [textView closestPositionToPoint:CGPointMake(48, 12)];
+    NSInteger offset = [textView offsetFromPosition:textView.beginningOfDocument toPosition:pos];
+    XCTAssertTrue(offset > 0, @"");
 }
 
 @end
+
+@interface NibCreatedUITextViewSelectableTrueTests : XCTestCase{
+    UITextView *textView;
+}
+@end
+
+@implementation NibCreatedUITextViewSelectableTrueTests
+
+- (void)setUp
+{
+    [super setUp];
+    textView = [[UINib nibWithNibName:@"TestUITextView" bundle:[NSBundle bundleForClass:self.class]] instantiateWithOwner:nil options:nil][0];
+    textView.attributedText = [[NSAttributedString alloc] initWithString:@"Hello World"];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testBeginingOfDocumentPositionShouldNotBeNil
+{
+    XCTAssertNotNil(textView.beginningOfDocument, @"");
+}
+
+- (void)testGetPositionofTextReturnsNotNil{
+    XCTAssertNotNil([textView closestPositionToPoint:CGPointMake(48, 12)], @"");
+}
+
+- (void)testGetOffsetNot0{
+    UITextPosition *pos = [textView closestPositionToPoint:CGPointMake(48, 12)];
+    NSInteger offset = [textView offsetFromPosition:textView.beginningOfDocument toPosition:pos];
+    XCTAssertTrue(offset > 0, @"");
+}
+
+@end
+
+@interface NibCreatedUITextViewSelectableFalseTests : XCTestCase{
+    UITextView *textView;
+}
+@end
+
+@implementation NibCreatedUITextViewSelectableFalseTests
+
+- (void)setUp
+{
+    [super setUp];
+    textView = [[UINib nibWithNibName:@"TestUITextView" bundle:[NSBundle bundleForClass:self.class]] instantiateWithOwner:nil options:nil][1];
+    textView.attributedText = [[NSAttributedString alloc] initWithString:@"Hello World"];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testBeginingOfDocumentPositionShouldNotBeNil
+{
+    XCTAssertNotNil(textView.beginningOfDocument, @"");
+}
+
+- (void)testGetPositionofTextReturnsNotNil{
+    XCTAssertNotNil([textView closestPositionToPoint:CGPointMake(48, 12)], @"");
+}
+
+- (void)testGetOffsetNot0{
+    UITextPosition *pos = [textView closestPositionToPoint:CGPointMake(48, 12)];
+    NSInteger offset = [textView offsetFromPosition:textView.beginningOfDocument toPosition:pos];
+    XCTAssertTrue(offset > 0, @"");
+}
+
+@end
+
